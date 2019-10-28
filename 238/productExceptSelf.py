3b68f09754c9eef250e6f3bbeb2e3efdc699abff
cccccccccccccc <chenzhang.1987@gmail.com>
2019-10-27 23:26:59 -0700

    Update productExceptSelf.py

--- 238/productExceptSelf.py
+++ 238/productExceptSelf.py
@@ -4,7 +4,11 @@ Created on Tue Jan  9 14:14:24 2018
 
 @author: chen
 """
-
+# time complexity = O(n) space complexity = O(1)
+# use 1 to help 
+# newlist = [1,nums,1]
+# product all the element on the left side
+# product all the element on the right side 
 class Solution(object):
     def productExceptSelf(self, nums):
         """
@@ -24,3 +28,18 @@ class Solution(object):
         return output
 A = Solution()
 print(A.productExceptSelf([3,2,1]))
+'''
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        outlist = []
+        tmpp = 1
+        nlen = len(nums)
+        for i in range(nlen):
+            outlist.append(tmpp)
+            tmpp*=nums[i]
+        tmpp = 1
+        for i in range((nlen-1),-1,-1):
+            outlist[i]*=tmpp
+            tmpp*= nums[i]
+        return outlist
+'''
