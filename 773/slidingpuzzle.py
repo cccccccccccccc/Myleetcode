e15a7ca3ef045301b415b5a495b1130c013e078c
cccccccccccccc <chenzhang.1987@gmail.com>
2020-05-16 09:48:25 -0700

    Update slidingpuzzle.py

--- 773/slidingpuzzle.py
+++ 773/slidingpuzzle.py
@@ -1,3 +1,10 @@
+from typing import List
+from collections import deque
 class Solution:
     def slidingPuzzle(self, board: List[List[int]]) -> int:
-        
+        foundstr = set()
+        originalstr = ""
+        possiblestatus = deque()
+        for m in range(len(board)):
+            for n in range(len(board[m])):
+                originalstr += str(board[m][n])
